72449f4a87c0a3d3efc2aca0d6d853f00ace58d9
ali-alim <ali.mamedov.2016@gmail.com>
2022-05-10 21:42:47 +0400

    create PostItem component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from "react";
+import PostItem from "./components/PostItem";
+import "./styles/App.css";
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      It is working
+    <div className="app">
+      <PostItem />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/PostItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PostItem = () => {
+  return (
+      <div className="post">
+        <strong>1. One</strong>
+        <div>One is #1</div>
+        <div className="post__btns">
+          <button>Delete</button>
+        </div>
+      </div>
+  );
+};
+
+export default PostItem;
